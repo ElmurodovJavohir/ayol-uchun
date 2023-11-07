aa94f613e289cdc9aee2d5d63e1be652a47695b3
bahromoken <magdiyevbahrom@gmail.com>
2023-11-07 10:21:47 +0500

    model course changes

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from blog.models import BlogImages
+
 
 # Create your models here.
+
 class Course(models.Model):
     title = models.CharField(max_length=255)
+    description = models.TextField()
+    photo = models.ForeignKey(BlogImages, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
