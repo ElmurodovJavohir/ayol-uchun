144d389b22dffeaa0c30a1f9d793cf4ddd819219
portfolio1 <fayzulloh00010002@gmail.com>
2023-11-07 10:03:10 +0500

    add urls.py

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-j
+
     'ckeditor',
     'ckeditor_uploader',
     'rest_framework',
