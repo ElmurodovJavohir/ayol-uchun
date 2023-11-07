01603db794cdf1eadead845a8feee481251f68ed
Shohruz Nuraddinov <92042320+shohruzcoder@users.noreply.github.com>
2023-11-07 13:33:44 +0500

    fix: admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,83 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-# Register your models here.
+from .models import User
+from .forms import UserCreationForm, UserChangeForm, UserPasswordChageForm
+
+
+class UserAdmin(BaseUserAdmin):
+    change_password_form = UserPasswordChageForm
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = [
+        'first_name',
+        'last_name',
+        "phone_number",
+        'is_superuser',
+        'is_active',
+        'email',
+    ]
+    list_filter = []
+    fieldsets = [
+        (None, {"fields": [
+            "password",
+            "phone_number",
+            'first_name',
+            'last_name',
+            'gender',
+            'is_superuser',
+            'is_active',
+            'birth_day',
+            'email',
+            'country',
+            'region',
+            'postcode',
+            'address',
+            'instagram',
+            'imkon_uz',
+            'linkedin',
+            'workplace',
+            'job',
+            'about',
+        ]}),
+    ]
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": [
+                    "phone_number",
+                    'first_name',
+                    'last_name',
+                    'gender',
+                    'is_superuser',
+                    'is_active',
+                    'birth_day',
+                    'email',
+                    'country',
+                    'region',
+                    'postcode',
+                    'address',
+                    'instagram',
+                    'imkon_uz',
+                    'linkedin',
+                    'workplace',
+                    'job',
+                    'about',
+                    "password",
+                ],
+            },
+        ),
+    ]
+    readonly_fields = ('phone_number',)
+    search_fields = ["phone_number", 'first_name', 'last_name']
+    ordering = ["id"]
+    filter_horizontal = []
+
+
+admin.site.register(User, UserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.validators import RegexValidator
+
 from .manager import UserManager
 # Create your models here.
 
@@ -16,19 +18,26 @@ class GenderChoise(models.Choices):
     FEMALE = 'Ayol'
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
+
+    _validate_phone = RegexValidator(
+        regex=r'^998[0-9]{9}$',
+        message="Telefon raqamingiz 9 bilan boshlanishi va 12 ta belgidan iborat bo'lishi kerak. Masalan: 998901235476"
+    )
     gender = models.CharField(
         max_length=255, choices=GenderChoise.choices, default=GenderChoise.FEMALE)
 
     first_name = models.CharField(max_length=50, null=True, blank=True)
     last_name = models.CharField(max_length=50, null=True, blank=True)
+    username = None
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
 
     birth_day = models.DateField(blank=True, null=True)
-    phone_number = models.CharField(max_length=15, unique=True)
+    phone_number = models.CharField(
+        max_length=15, unique=True, validators=[_validate_phone])
     email = models.EmailField(null=True, blank=True)
 
     country = models.CharField(max_length=255, blank=True, null=True)
@@ -49,6 +58,9 @@ class User(AbstractBaseUser):
     USERNAME_FIELD = 'phone_number'
 
     objects = UserManager()
+
+    def get_full_name(self):
+        return self.first_name + ' ' + self.last_name
 
     def __str__(self) -> str:
         return self.get_full_name()
