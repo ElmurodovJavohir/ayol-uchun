758ea9080f9a5e3cebcf69d7bb40dedc50f31709
Khazratbek <100770676+Xazratbek@users.noreply.github.com>
2023-11-07 09:43:04 +0500

    adding blog app

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,45 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from ckeditor.fields import RichTextField
+
+
+class BlogCategory(models.Model):
+    title = models.CharField(max_length=128)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class BlogImages(models.Model):
+    image = models.ImageField(upload_to="blog_images")
+
+    def __str__(self) -> str:
+        return str(self.id)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = RichTextField()
+    photo = models.ForeignKey(BlogImages, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="comments"
+    )
+    comment = models.CharField(max_length=150)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.comment
